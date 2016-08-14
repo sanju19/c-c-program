795672fe29266b7ea4662960d8d88d4d9f40a10f
sanju19 <sonamsanju1991@gmail.com>
2016-08-14 14:25:59 +0530

    Create lower to upper.c

---
+++ lower to upper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main()
+{
+  char str[20];
+  int i;
+  printf("Enter any string: ");
+  scanf("%s",str);
+  printf("The string is: %s",str);
+  for(i=0;i<=strlen(str);i++)
+    {
+    if(str[i]>=97&&str[i]<=122)
+         str[i]=str[i]-32;
+    }
+  printf("\nThe string in uppercase is: %s",str);
+  return 0;
+}
