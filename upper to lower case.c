e81b2d51f523dba1747ae3b15b631e99f35c8fbf
sanju19 <sonamsanju1991@gmail.com>
2016-08-14 14:21:45 +0530

    Create upper to lower case.c

---
+++ upper to lower case.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main()
+{
+  char str[20];
+  int i;
+  printf("Enter any string: ");
+  scanf("%s",str);
+  printf("The string is: %s",str);
+  for(i=0;i<=strlen(str);i++)
+  {
+      if(str[i]>=65&&str[i]<=90)
+       str[i]=str[i]+32;
+  }
+  printf("\nThe string in lowercase is: %s",str);
+  return 0;
+}
