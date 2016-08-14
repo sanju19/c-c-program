8d5d95b577b49e1372c66d4a37b47c12636699d7
sanju19 <sonamsanju1991@gmail.com>
2016-08-14 14:08:36 +0530

    Create ascii value.c

---
+++ ascii value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    printf("Enter any character: ");
+    scanf("%c",&c);
+    printf("ASCII value of given character: %d",c);
+    return 0;
+}
